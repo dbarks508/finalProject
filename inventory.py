19723e03252c99609234b9b68bd774b64d3249aa
Dylan Barks <dylan.barks95@gmail.com>
2022-07-20 17:05:55 -0400

    small things

--- inventory.py
+++ inventory.py
@@ -49,7 +49,6 @@ class Inventory():
     # search for one attribute and one value with class attributes -- not sure which is better
     def search(self):
         readIn()
-
         return list(filter(lambda item: item[self.searchKey] == self.searchValue, carList))
 
 '''|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'''
